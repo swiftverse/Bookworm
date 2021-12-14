ebb044ac4a893fd1c8dfa48507e6113ce34ece53
shivgirja <amitsrivastava115@yahoo.com>
2021-12-14 15:32:50 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Amit Shrivastava on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
